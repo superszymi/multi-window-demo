0271da37db6ec6adda0802568d57b2ac7a47554e
Szymon Rogalski <szymon.rogalski@stxnext.pl>
2022-11-30 13:24:20 +0100

    feat: multiple counters

--- src/App.js
+++ src/App.js
@@ -1,56 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useCallback } from "react";
+import Counter from "./features/counter/Counter";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  addCounter,
+  selectCounters,
+  selectFirstFreeId,
+} from "./features/counter/counterSlice";
 
 function App() {
+  const counters = useSelector(selectCounters);
+  const nextId = useSelector(selectFirstFreeId);
+  const dispatch = useDispatch();
+
+  const onAddCounter = useCallback(() => {
+    dispatch(addCounter(nextId));
+  }, [dispatch, nextId]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={onAddCounter}>Add counter</button>
+        <button onClick={() => dispatch(increment())}>+</button>
       </header>
+      <main className="App-main">
+        {counters.map((id) => (
+          <Counter key={id} id={id} />
+        ))}
+      </main>
     </div>
   );
 }
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,67 +1,27 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeCounter, selectCount } from "./counterSlice";
+import styles from "./Counter.module.css";
 
-export function Counter() {
+const Counter = ({ id }) => {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
 
-  const incrementValue = Number(incrementAmount) || 0;
+  const onRemove = () => {
+    dispatch(removeCounter(id));
+  };
 
   return (
     <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
+      <div className={styles.counter}>
+        <button className={styles.remove} onClick={onRemove}>
+          x
         </button>
+        <span className={styles.title}>Counter {id}</span>
         <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
       </div>
     </div>
   );
-}
+};
+
+export default Counter;
